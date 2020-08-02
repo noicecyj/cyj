1ba1b558f14c24f9cc450424c093e5a0ff6dbdbb
yuanjie cao <952637212@qq.com>
2020-08-02 23:43:13 +0800

    升级啦

---
+++ cyj-log/src/main/java/com/example/cyjlog/CyjLogApplication.java
@@ -0,0 +1,22 @@
+package com.example.cyjlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author 曹元杰
+ * @version 1.0
+ * @date 2020/1/21 14:46
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class CyjLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyjLogApplication.class, args);
+    }
+
+}
