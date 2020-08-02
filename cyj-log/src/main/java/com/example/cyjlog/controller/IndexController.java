6553ac03ec85a66e1954cdfc1d4e05a47168664f
yuanjie cao <952637212@qq.com>
2020-08-02 23:55:07 +0800

    升级啦

---
+++ cyj-log/src/main/java/com/example/cyjlog/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.cyjlog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author 曹元杰
+ * @version 1.0
+ * @date 2020/1/21 14:46
+ */
+@RestController
+@RequestMapping(value = "logApi")
+public class IndexController {
+
+}
