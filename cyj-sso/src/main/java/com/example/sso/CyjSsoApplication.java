57ef67eff14358777889f0f3aaed4ef0eef95aab
yuanjie cao <952637212@qq.com>
2021-02-27 20:38:19 +0800

    升级啦

--- cyj-sso/src/main/java/com/example/sso/CyjSsoApplication.java
+++ cyj-sso/src/main/java/com/example/sso/CyjSsoApplication.java
@@ -3,6 +3,7 @@ package com.example.sso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableZuulProxy
 public class CyjSsoApplication {
 
     public static void main(String[] args) {
