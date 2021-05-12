16912cb22dabbe20077fb6e42fa03547c70ae986
noicecyj <952637212@qq.com>
2021-05-13 01:26:01 +0800

    升级啦

--- cyj-user/src/main/java/com/example/CyjUser/config/AuthorizationServerConfig.java
+++ cyj-user/src/main/java/com/example/CyjUser/config/AuthorizationServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * @author 曹元杰
@@ -38,59 +39,34 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         this.userService = userService;
     }
 
-    /**
-     * 使用密码模式需要配置
-     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager)
-                .userDetailsService(userService);
+        endpoints.userDetailsService(userService)
+                .authenticationManager(authenticationManager);
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                //配置client_id
-                .withClient("cyj-dictionary")
-                //配置client-secret
-                .secret(passwordEncoder.encode("admin123456"))
-                //配置访问token的有效期
-                .accessTokenValiditySeconds(3600)
-                //配置刷新token的有效期
-                .refreshTokenValiditySeconds(864000)
-                //配置redirect_uri，用于授权成功后跳转
-                .redirectUris("http://www.baidu.com")
-                //配置申请的权限范围
-                .scopes("all")
-                //配置grant_type，表示授权类型
-                .authorizedGrantTypes("authorization_code", "password")
-                .and()
+                //应用ID
                 .withClient("cyj-entity-creater")
-                //配置client-secret
-                .secret(passwordEncoder.encode("admin123456"))
-                //配置访问token的有效期
-                .accessTokenValiditySeconds(3600)
-                //配置刷新token的有效期
-                .refreshTokenValiditySeconds(864000)
-                //配置redirect_uri，用于授权成功后跳转
-                .redirectUris("http://www.baidu.com")
-                //配置申请的权限范围
-                .scopes("all")
-                //配置grant_type，表示授权类型
-                .authorizedGrantTypes("authorization_code", "password")
-                .and()
-                .withClient("cyj-user")
-                //配置client-secret
-                .secret(passwordEncoder.encode("admin123456"))
-                //配置访问token的有效期
-                .accessTokenValiditySeconds(3600)
-                //配置刷新token的有效期
-                .refreshTokenValiditySeconds(864000)
-                //配置redirect_uri，用于授权成功后跳转
-                .redirectUris("http://www.baidu.com")
-                //配置申请的权限范围
-                .scopes("all")
-                //配置grant_type，表示授权类型
-                .authorizedGrantTypes("authorization_code", "password");
+                //应用密码
+                .secret(passwordEncoder.encode("123456"))
+                //应用权限
+                .scopes("read", "write")
+                //token的失效时间
+                .accessTokenValiditySeconds(1800)
+                //允许访问的服务ID
+                .resourceIds("order-server")
+                //授权方式
+                .authorizedGrantTypes("password");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        //暴露验证服务/oauth/token_key   permitAll()为关闭
+        security.tokenKeyAccess("isAuthenticated()")
+                //暴露验证服务/oauth/check_token
+                .checkTokenAccess("isAuthenticated()");
     }
 }
--- cyj-user/src/main/java/com/example/CyjUser/config/SecurityConfig.java
+++ cyj-user/src/main/java/com/example/CyjUser/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.CyjUser.config;
 
+import com.example.CyjUser.serviceimpl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,28 +21,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private UserServiceImpl userService;
+
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+    /**
+     * 1:
+     * 请求授权:
+     * spring security 使用以下匹配器来匹配请求路劲：
+     *      antMatchers:使用ant风格的路劲匹配
+     *      regexMatchers:使用正则表达式匹配路劲
+     * anyRequest:匹配所有请求路劲
+     * 在匹配了请求路劲后，需要针对当前用户的信息对请求路劲进行安全处理。
+     * 2:定制登录行为。
+     *      formLogin()方法定制登录操作
+     *      loginPage()方法定制登录页面访问地址
+     *      defaultSuccessUrl()登录成功后转向的页面
+     *      permitAll()
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/static/**").permitAll()
+                .antMatchers("/register").permitAll()
+                .anyRequest().authenticated()//其余所有请求都需要认证后才可访问
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll();//允许用户任意访问
+        http.csrf().disable();
     }
 
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/oauth/**", "/login/**", "/logout/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .permitAll();
     }
 }
